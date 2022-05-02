073897272936fac03dcb356ff66205a7c8ebd5d3
FlutLab.io <>
2022-05-02 14:26:25 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ejemplo.ListVew', // pestaña en web
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ), //tema color global
+        home: const MyHomePage());
+  }
+} // widget sin estado
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+} // widget por estado
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/s1.jpg",
+    "assets/images/s2.jpg",
+    "assets/images/s3.jpg",
+    "assets/images/s4.jpg",
+    "assets/images/s5.jpg",
+    "assets/images/s6.jpg",
+    "assets/images/s7.jpg",
+    "assets/images/s8.jpg",
+  ]; // lista de imagenes
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Catalogo De Soriana"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
